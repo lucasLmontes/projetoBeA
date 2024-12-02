6bea96def3047f2bfa009add2a0f09a70b4095b3
Lucas Leite <lucasleite2mil4@gmail.com>
2024-12-02 17:50:13 -0300

    cadastro

--- src/main/java/org/example/bea/repository/UsuarioRepository.java
+++ src/main/java/org/example/bea/repository/UsuarioRepository.java
@@ -3,5 +3,7 @@ package org.example.bea.repository;
 import org.example.bea.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 }
